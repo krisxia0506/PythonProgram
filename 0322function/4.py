5b4cb8ed7fefc2d20b59aaa56e1d5eafd57c8ae2
krisxia0506 <xiajiayi0506@gmail.com>
2023-03-22 22:08:45 +0800

    函数和文件操作

--- 0322function/4.py
+++ 0322function/4.py
@@ -1,39 +1,36 @@
 def add(a, b):
-    carry = 0
-    res = []
-    i = len(a) - 1
-    j = len(b) - 1
-    while i >= 0 or j >= 0 or carry > 0:
-        num1 = int(a[i]) if i >= 0 else 0
-        num2 = int(b[j]) if j >= 0 else 0
-        s = num1 + num2 + carry
-        res.append(str(s % 10))
-        carry = s // 10
-        i -= 1
-        j -= 1
-    return ''.join(res[::-1])
+    # 将两个数字用0补齐，使得它们的长度相等
+    n = max(len(a), len(b))
+    a = a.zfill(n)
+    b = b.zfill(n)
+    # 从低位到高位依次计算每一位的和
+    carry = 0  # 进位
+    res = ''
+    for i in range(n - 1, -1, -1):
+        digit_sum = int(a[i]) + int(b[i]) + carry
+        carry = digit_sum // 10
+        res = str(digit_sum % 10) + res
+    # 如果最高位有进位，则在结果前加上1
+    if carry > 0:
+        res = '1' + res
+    return res
 
 
 def multiply(a, b):
-    m = len(a)
-    n = len(b)
-    res = [0] * (m + n)
-    for i in range(m - 1, -1, -1):
-        carry = 0
-        for j in range(n - 1, -1, -1):
-            s = int(a[i]) * int(b[j]) + carry + res[i + j + 1]
-            res[i + j + 1] = s % 10
-            carry = s // 10
-        res[i] += carry
-    res = ''.join(map(str, res)).lstrip('0')
+    # 将两个数字转换为整数
+    a = int(a)
+    b = int(b)
+    # 计算乘积
+    prod = a * b
+    res = str(prod)
     return res if res else '0'
 
 
 # 读入输入文件
 with open('plus.in', 'r') as f:
-    a = f.readline().strip()
-    op = f.readline().strip()
-    b = f.readline().strip()
+    a = f.readline().strip()  # 第一行是超长正整数A
+    op = f.readline().strip()  # 第二行只有一个字符"+"或"*"，分别代表加、乘运算
+    b = f.readline().strip()  # 第三行是超长正整数B
 
 # 进行运算
 if op == '+':
