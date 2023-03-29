b039c34bc4dcdfbad76fc5e03636c7c0a492b702
krisxia0506 <xiajiayi0506@gmail.com>
2023-03-29 20:08:34 +0800

    面向对象

--- 0329oop/1.py
+++ 0329oop/1.py
@@ -2,13 +2,13 @@ class Rect():
     def __init__(self, len, wid):
         self.len = len
         self.wid = wid
+
     def area(self):
-        area = self.len *self.wid
+        area = self.len * self.wid
         return area
+
 
 x = float(input())
 y = float(input())
 r1 = Rect(x, y)
 print(r1.area())
-
-
